f8bb509307c60c5d435cc6567de65dcb23f3c421
Simone Meneghin <simone.meneghin.98@outlook.it>
2020-04-23 18:43:48 +0200

    Primo test

---
+++ prova/src/components/header.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+
+    render() {
+        return (
+            <h1 className="text-light text-center header">Tool di addestramento</h1>
+        );
+    }
+}
+
+export default Header;
