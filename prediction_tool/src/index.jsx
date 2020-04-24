4625d8debbd797c8a360545fd3e267710ff7fcca
Davide Zilio <ziliodav97@gmail.com>
2020-04-24 21:18:54 +0200

    tolti file inutili e aggiunto App all'index

--- prediction_tool/src/index.jsx
+++ prediction_tool/src/index.jsx
@@ -4,11 +4,13 @@ import "bootstrap/dist/css/bootstrap.css"
 import "./index.css"
 import Header from "./components/header";
 import States from "./components/state_controller"
+import App from "./davide-fouad/App";
 
 
 ReactDom.render(
     <React.StrictMode>
         <Header/>
+        <App />
         <States/>
     </React.StrictMode>,
     document.getElementById("root")
