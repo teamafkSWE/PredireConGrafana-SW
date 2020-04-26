a7f6586a70cc352929006ea3a3d942b510e27ee4
Davide Zilio <ziliodav97@gmail.com>
2020-04-26 13:55:02 +0200

    tolto file App e integrato contenuto in index

--- prediction_tool/src/index.jsx
+++ prediction_tool/src/index.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import ReactDom from 'react-dom';
 import "bootstrap/dist/css/bootstrap.css"
 import "./index.css"
-import App from "./App";
+import Header from "./components/header";
+import States from "./components/state_controller";
+import Select_Prediction from "./components/chooseAlgorithm";
 
 
 ReactDom.render(
     <React.StrictMode>
-
-    <App/>
+        <div className="mt-4 mb-4 text-center" >
+            <Header/>
+            <States/>
+            <Select_Prediction/>
+        </div>
     </React.StrictMode>,
     document.getElementById("root")
 );
