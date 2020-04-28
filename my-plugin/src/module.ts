fa7a5cbc9c34ec5c71b15eb78712563ef32e9f6a
Simone <simone.meneghin.98@outlook.it>
2020-04-28 14:54:23 +0200

    Ri-aggiunta pagina di config

--- my-plugin/src/module.ts
+++ my-plugin/src/module.ts
@@ -1,10 +1,14 @@
 // Angular pages
+import {ExampleConfigCtrl} from 'legacy/config'
 import {AppPlugin} from '@grafana/data';
 import {ExamplePage1} from './config/ExamplePage1';
 import {ExamplePage2} from './config/ExamplePage2';
 import {ExampleRootPage} from './ExampleRootPage';
 import {ExampleAppSettings} from './types';
 
+export {
+    ExampleConfigCtrl as ConfigCtrl,
+};
 
 export const plugin = new AppPlugin<ExampleAppSettings>()
   .setRootPage(ExampleRootPage)
