05a4a68fc68465dbe968e2818eb9c3c40d355b1a
Simone <simone.meneghin.98@outlook.it>
2020-04-28 14:22:08 +0200

    rimozione file Angular

--- my-plugin/src/module.ts
+++ my-plugin/src/module.ts
@@ -1,17 +1,10 @@
 // Angular pages
-import { ExampleConfigCtrl } from './legacy/config';
-import { AngularExamplePageCtrl } from './legacy/angular_example_page';
-import { AppPlugin } from '@grafana/data';
-import { ExamplePage1 } from './config/ExamplePage1';
-import { ExamplePage2 } from './config/ExamplePage2';
-import { ExampleRootPage } from './ExampleRootPage';
-import { ExampleAppSettings } from './types';
+import {AppPlugin} from '@grafana/data';
+import {ExamplePage1} from './config/ExamplePage1';
+import {ExamplePage2} from './config/ExamplePage2';
+import {ExampleRootPage} from './ExampleRootPage';
+import {ExampleAppSettings} from './types';
 
-// Legacy exports just for testing
-export {
-  ExampleConfigCtrl as ConfigCtrl,
-  AngularExamplePageCtrl, // Must match `pages.component` in plugin.json
-};
 
 export const plugin = new AppPlugin<ExampleAppSettings>()
   .setRootPage(ExampleRootPage)
