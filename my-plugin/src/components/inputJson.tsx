ba205f7bb6e4b3b5fd31717aa1f7e5ae8736b7d6
Simone <simone.meneghin.98@outlook.it>
2020-05-01 16:52:26 +0200

    Aggiunta lettura file

--- my-plugin/src/components/inputJson.tsx
+++ my-plugin/src/components/inputJson.tsx
@@ -1,8 +1,24 @@
 import React, {Component} from 'react';
+import Files from 'react-files'
+
 
 
 class InsJson extends Component {
+    private fileReader: any;
+    state = {
+        jsonFile: {}
+    }
 
+    constructor(props: Readonly<{}>) {
+        super(props);
+        this.fileReader = new FileReader();
+        this.fileReader.onload = (event: { target: { result: string; }; }) => {
+            this.setState({ jsonFile: JSON.parse(event.target.result) }, () => {
+                console.log(this.state.jsonFile);
+            });
+        };
+    }
+    /*
     onChange=(e:any)=>{
         let file=e.target.files[0];
         console.log(file);
@@ -15,10 +31,24 @@ class InsJson extends Component {
 
         })*/
        // console.log(JSON.parse(file));
-        }
+     //   }
     render() {
         return (
-            <input type='file' name='file' onChange={(e)=>this.onChange(e)}/>
+            <Files
+                className="files-dropzone"
+                onChange={(file: any[]) => {
+                    this.fileReader.readAsText(file[0]);
+                }}
+                onError={(err: any) => console.log(err)}
+                accepts={[".json"]}
+                multiple
+                maxFiles={3}
+                maxFileSize={10000000}
+                minFileSize={0}
+                clickable
+            >
+                Drop files here or click to upload
+            </Files>
         );
     }
 }
