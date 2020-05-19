316d498f212c24a1917114e67c6fd680555fe23f
Victor <victor.dutca@studenti.unipd.it>
2020-05-19 14:37:47 +0200

    Cambiati titoli tool

--- prediction_tool/src/components/header.jsx
+++ prediction_tool/src/components/header.jsx
@@ -5,8 +5,8 @@ class Header extends Component {
     render() {
         return (
             <div className="header mb-2">
-                <h1 className="text-white text-center">Prediction Tool</h1>
-                <h2 className="text-dark text-center"><small>get your prediction here!</small></h2>
+                <h1 className="text-white text-center">Training Tool</h1>
+                <h2 className="text-dark text-center"><small>make your data useful</small></h2>
             </div>
         );
     }
