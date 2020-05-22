7a96909ccff0ebe270382147b5f1a97caefd156e
farid <foadfffarid@gmail.com>
2020-05-22 19:30:23 +0200

    grafico SVM

--- prediction_tool/src/view/uiComponents/chart.jsx
+++ prediction_tool/src/view/uiComponents/chart.jsx
@@ -4,9 +4,17 @@ import {Scatter} from "react-chartjs-2";
 class Chart extends Component{
 
     state={
-        options : {
-            datasets: []
+        data : {
 
+            datasets: [],
+            legend: {
+                display: false,
+            }
+        },
+        options:{
+            legend: {
+                display: false,
+            }
         }
     }
     dynamicColors = ()=> {
@@ -18,28 +26,46 @@ class Chart extends Component{
     formatData=()=>{
 
         if (this.props.hasFile!==false) {
-           
-            this.state.options.datasets=[];
+            this.state.data.datasets=[];
             let propData = this.props.data;
+            if(propData[0][propData[0].length - 1] === "y") {
                 for (let i = 0; i < propData[0].length - 1; i++) {
-                    let setData={
+                    let setData = {
                         label: propData[0][i], // Name the series
                         data: [], // Specify the data values array
-
                         backgroundColor: this.dynamicColors(), // Add custom color background (Points and Fill)
                     };
-                    for (let j = 1; j < propData.length ; j++){
-
-                        setData.data.push({x: propData[j][i], y: propData[j][propData[0].length-1]});
+                    for (let j = 1; j < propData.length; j++) {
+                        setData.data.push({x: propData[j][i], y: propData[j][propData[0].length - 1]});
                     }
-                    this.state.options.datasets.push(setData);
+                    this.state.data.datasets.push(setData);
                 }
+                this.state.options.legend.display=true;
+            }
+            if(propData[0][propData[0].length - 1] === "label") {
+                for (let i = 0; i < propData[0].length - 2; i++) {
+                    let setData = {
+                        data: [], // Specify the data values array
+                        backgroundColor: []
+                        }
+                    for (let j = 1; j < propData.length; j++) {
+                        if(propData[j][propData[0].length - 1]==="1")
+                            setData.backgroundColor.push("green");
+
+                        else
+                            setData.backgroundColor.push("red");
+                        setData.data.push({x: propData[j][i], y: propData[j][propData[0].length - 2]});
+                    }
+                    this.state.data.datasets.push(setData);
+                }
+                this.state.options.legend.display=false;
+            }
         }
     }
     render() {
         this.formatData();
         return(
-            <Scatter data={this.state.options}/>
+            <Scatter data={this.state.data} options={this.state.options}/>
         );
     }
 }
