d04145227eee495c6d67840bcfd2db30abbaa82a
farid <foadfffarid@gmail.com>
2020-05-24 22:24:23 +0200

    migliorata struttura Chart
    migliorata struttura Chart

--- prediction_tool/src/view/uiComponents/chart.jsx
+++ prediction_tool/src/view/uiComponents/chart.jsx
@@ -40,93 +40,83 @@ class Chart extends Component{
              pointRadius:0
          })
     }
+    RLChart=(propData)=>{
+        let lowest = Number.POSITIVE_INFINITY;
+        let highest = Number.NEGATIVE_INFINITY;
+        for (let i = 0; i < propData[0].length - 1; i++) {
+            let setData = {
+                label: propData[0][i], // Name the series
+                data: [], // Specify the data values array
+                backgroundColor: this.dynamicColors(), // Add custom color background (Points and Fill)
+            };
+            for (let j = 1; j < propData.length; j++) {
 
-    formatData=()=>{
-        if (this.props.hasFile!==false && this.props.json!==null) {
-            this.state.data.datasets=[];
+                console.log(propData[j][i])
+                console.log(lowest)
+
+                lowest = Math.min(propData[j][i], lowest)
+                highest = Math.max(propData[j][i], highest);
+                setData.data.push({x: propData[j][i], y: propData[j][propData[0].length - 1]});
+
+            }
+
+            console.log(highest)
+            console.log(lowest)
+
+            this.state.data.datasets.push(this.straightLine(propData[0][i],highest,lowest,i));
+            lowest = Number.POSITIVE_INFINITY;
+            highest = Number.NEGATIVE_INFINITY;
+
+            this.state.data.datasets.push(setData);
+        }
+        this.state.options.legend.display = true;
+    }
+    SVMChart=(propData)=>{
+        if(propData[0].length===2)
+        {
+            let setData = {
+                label: propData[0][0], // Name the series
+                data: [], // Specify the data values array
+                backgroundColor: [],
+
+            }
+            for (let j = 1; j < propData.length; j++) {
+                if(propData[j][propData[0].length - 1]==="1")
+                    setData.backgroundColor.push("green");
+                else
+                    setData.backgroundColor.push("red");
+                setData.data.push({x: propData[j][0], y: 0});
+            }
+            this.state.data.datasets.push(setData);
+            this.state.options.legend.display = false;
+        }
+        else {
+            for (let i = 0; i < propData[0].length - 2; i++) {
+                let setData = {
+                    label: propData[0][i], // Name the series
+                    data: [], // Specify the data values array
+                    backgroundColor: [],
+                }
+                for (let j = 1; j < propData.length; j++) {
+                    if (propData[j][propData[0].length - 1] === "1")
+                        setData.backgroundColor.push("green");
+                    else
+                        setData.backgroundColor.push("red");
+                    setData.data.push({x: propData[j][i], y: propData[j][propData[0].length - 2]});
+                }
+                this.state.data.datasets.push(setData);
+            }
+            this.state.options.legend.display = false;
+        }
+    }
+    formatData=()=> {
+        if (this.props.hasFile !== false && this.props.json !== null) {
+            this.state.data.datasets = [];
             let propData = this.props.data;
-            if(propData[0][propData[0].length - 1] === "y") {
-                let lowest = Number.POSITIVE_INFINITY;
-                let highest = Number.NEGATIVE_INFINITY;
-                for (let i = 0; i < propData[0].length - 1; i++) {
-                    let setData = {
-                        label: propData[0][i], // Name the series
-                        data: [], // Specify the data values array
-                        backgroundColor: this.dynamicColors(), // Add custom color background (Points and Fill)
-                    };
-                    for (let j = 1; j < propData.length-1; j++) {
-
-                        console.log(propData[j][i])
-                        console.log(lowest)
-
-                        lowest = Math.min(propData[j][i], lowest)
-                        highest = Math.max(propData[j][i], highest);
-                        setData.data.push({x: propData[j][i], y: propData[j][propData[0].length - 1]});
-
-                    }
-
-                    console.log(highest)
-                    console.log(lowest)
-
-                    this.state.data.datasets.push(this.straightLine(propData[0][i],highest,lowest,i));
-                    lowest = Number.POSITIVE_INFINITY;
-                    highest = Number.NEGATIVE_INFINITY;
-
-                    this.state.data.datasets.push(setData);
-                }
-                this.state.options.legend.display = true;
-            }
-            if(propData[0][propData[0].length - 1] === "label") {
-
-                if(propData[0].length===2)
-                {
-                    let setData = {
-
-                        label: propData[0][0], // Name the series
-                        data: [], // Specify the data values array
-                        backgroundColor: [],
-
-                    }
-
-                    for (let j = 1; j < propData.length; j++) {
-
-                        if(propData[j][propData[0].length - 1]==="1")
-                            setData.backgroundColor.push("green");
-
-                        else
-                            setData.backgroundColor.push("red");
-
-                        setData.data.push({x: propData[j][0], y: 0});
-                    }
-                    this.state.data.datasets.push(setData);
-                    this.state.options.legend.display = false;
-                }
-                else {
-                    for (let i = 0; i < propData[0].length - 2; i++) {
-
-                        let setData = {
-                            label: propData[0][i], // Name the series
-                            data: [], // Specify the data values array
-                            backgroundColor: [],
-
-                        }
-
-                        for (let j = 1; j < propData.length; j++) {
-
-                            if (propData[j][propData[0].length - 1] === "1")
-                                setData.backgroundColor.push("green");
-
-                            else
-                                setData.backgroundColor.push("red");
-
-                            setData.data.push({x: propData[j][i], y: propData[j][propData[0].length - 2]});
-                        }
-                        this.state.data.datasets.push(setData);
-                    }
-                    this.state.options.legend.display = false;
-                }
-            }
-
+            if (propData[0][propData[0].length - 1] === "y")
+                this.RLChart(propData);
+            else if (propData[0][propData[0].length - 1] === "label")
+                this.SVMChart(propData);
         }
     }
     render() {
