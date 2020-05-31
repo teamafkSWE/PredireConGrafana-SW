d4a1afdc5bc9b9a67d26920ebbb43989ab980ca8
Simone <simone.meneghin.98@outlook.it>
2020-05-31 19:10:20 +0200

    started implementing predictions

--- my-plugin/src/controller/controller.ts
+++ my-plugin/src/controller/controller.ts
@@ -1,30 +1,50 @@
 import {Predictor} from "../types";
 import Observable from "./observable";
+import Algorithm from "../model/algorithm";
+import {Svm, SvmData} from "../model/algorithms/svm";
+import {Regression, RLData} from "../model/algorithms/regression";
 
 
-export default class Controller extends Observable{
+export default class Controller extends Observable {
     private _json: any
     private _file: File | undefined
-    private _predictors:Predictor[] = []
+    private _predictors: Predictor[] = []
     private _b: number | undefined;
+    private _algorithm: Algorithm | undefined;
 
     private _definePredictors = () => {
-        this._predictors=[];
+        this._predictors = [];
         const rawPredictors = this._json.predictors
         const rawValues = this._json.result
-        const predictorsNames:string[] = []
-        const predictorsValues:number[] = []
-        if (this._json.algorithm === "Linear Regression"){
+        const predictorsNames: string[] = []
+        const predictorsValues: number[] = []
+        if (this._json.algorithm === "Linear Regression") {
             rawPredictors.a.forEach((name: string) => predictorsNames.push(name))
             rawValues.a.forEach((value: number) => predictorsValues.push(value))
-        }
-        else if (this._json.algorithm === "SVM"){
+        } else if (this._json.algorithm === "SVM") {
             rawPredictors.w.forEach((name: string) => predictorsNames.push(name))
             rawValues.w.forEach((value: number) => predictorsValues.push(value))
         }
 
-        for (let i = 0; i < predictorsNames.length; i++){
+        for (let i = 0; i < predictorsNames.length; i++) {
             this._predictors.push({name: predictorsNames[i], value: predictorsValues[i]})
+        }
+    }
+
+    private _setStrategy() {
+        const algorithm: string = this._json.algorithm
+        if (algorithm === 'Linear Regression') {
+            const data: RLData = {
+                a: this._json.result.a,
+                b: this._json.result.b
+            }
+            this._algorithm = new Regression(data);
+        } else if (algorithm === 'SVM') {
+            const data: SvmData = {
+                wheits: this._json.result.w,
+                bias: this._json.result.b
+            }
+            this._algorithm = new Svm(data);
         }
     }
 
@@ -35,6 +55,7 @@ export default class Controller extends Observable{
             if (event.target !== null && typeof event.target.result === "string") {
                 this._json = JSON.parse(event.target.result);
                 this._definePredictors()
+                this._setStrategy()
                 this._b = this._json.result.b
                 this.notifyAll()
             }
@@ -57,6 +78,12 @@ export default class Controller extends Observable{
 
     public getB = () => {
         return this._b
+    }
+
+    public getPrediction = (inputs: number[]) => {
+        if (this._algorithm !== undefined)
+            return this._algorithm.predict(inputs);
+        return
     }
 }
 
