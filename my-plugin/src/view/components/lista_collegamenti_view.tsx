ac35ebce2f0d2a91cbb81205394a089eb7f0fe08
farid <foadfffarid@gmail.com>
2020-06-07 13:10:55 +0200

    fix button elimina collegamento
    fix button elimina collegamento

--- my-plugin/src/view/components/lista_collegamenti_view.tsx
+++ my-plugin/src/view/components/lista_collegamenti_view.tsx
@@ -1,34 +1,42 @@
 import React, {PureComponent} from 'react';
 import {PanelOptionsGrid, PanelOptionsGroup, VerticalGroup, HorizontalGroup} from "@grafana/ui";
 import Controller from "../../controller/controller";
-import Observer from "./observer/observer";
+//import Observer from "./observer/observer";
 //import {DataFrame} from "@grafana/data";
 
 interface MyProps {
     controller: Controller
 }
 
-class ListaCollegamentiView extends PureComponent<MyProps> implements Observer{
-    update(): void {
-        this.forceUpdate();
+class ListaCollegamentiView extends PureComponent<MyProps>{
+    state={
+        listConnection:[] as any
+    }
+    constructor(props: Readonly<MyProps>) {
+        super(props);
+        console.log(this.props.controller.getConnections())
+       this.state.listConnection=this.props.controller.getConnections()
+        console.log(this.state.listConnection)
     }
 
     handleDelete=(e:any)=>{
         if(confirm("Scollegare il collegamento?")){
             this.props.controller.removeListPredictorQuery(e.target.id);
+            this.setState({listConnection:this.props.controller.getConnections()});
+            console.log(this.state.listConnection)
+            this.forceUpdate();
         }
     }
-
     showConnection=()=>{
-        let objNameList=this.props.controller.getConnections();
+        let listConn=this.state.listConnection;
         let viewNameList=[];
-        if(objNameList.length=== 0)
+        if(listConn.length=== 0)
             return <label style={{fontStyle: "italic"}}>Nessun collegamento inserito.</label>
         else {
-            for (let i=0;i<objNameList.length;i++) {
-                let id=objNameList[i].id;
-                let name=objNameList[i].name;
-                let list=objNameList[i].queries;
+            for (let i=0;i<listConn.length;i++) {
+                let id=listConn[i].id;
+                let name=listConn[i].name;
+                let list=listConn[i].queries;
                 viewNameList.push(
                     <div>
                         <HorizontalGroup>
