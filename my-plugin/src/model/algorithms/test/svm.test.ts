2c9b6e1018d262b84b43916020c107f3d5892292
Simone <simone.meneghin.98@outlook.it>
2020-06-15 15:48:55 +0200

    correzione tests

--- my-plugin/src/model/algorithms/test/svm.test.ts
+++ my-plugin/src/model/algorithms/test/svm.test.ts
@@ -1,56 +1,53 @@
-import {Svm, SvmData} from "../svm";
+import {Svm} from "../svm";
 
 it('should be ok', function () {
-    const data: SvmData = {
-        bias: 1.363618234217184,
-        wheits: [
-            -0.032274505797868946,
-            -0.021486578304954727,
-            -0.0542787381049902
-        ]
-    }
-    const SVM = new Svm(data)
+
+    const bias = 1.363618234217184
+    const wheits = [
+        -0.032274505797868946,
+        -0.021486578304954727,
+        -0.0542787381049902
+    ]
+
+    const SVM = new Svm(wheits, bias)
     const input = [-0.4326, 11.909, 3]
     expect(SVM.predict(input)).toStrictEqual(1)
 });
 it('should be ok', function () {
-    const data: SvmData = {
-        bias: 1.363618234217184,
-        wheits: [
-            -0.032274505797868946,
-            -0.021486578304954727,
-            -0.0542787381049902
-        ]
-    }
-    const SVM = new Svm(data)
+    const bias = 1.363618234217184
+    const weights = [
+        -0.032274505797868946,
+        -0.021486578304954727,
+        -0.0542787381049902
+    ]
+
+    const SVM = new Svm(weights, bias)
     const input = [-11.465, 0.1746, 6]
     expect(SVM.predict(input)).not.toBe(-1)
 });
 
 it('should throw error - less', function () {
-    const data: SvmData = {
-        bias: 1.363618234217184,
-        wheits: [
-            -0.032274505797868946,
-            -0.021486578304954727,
-            -0.0542787381049902
-        ]
-    }
-    const SVM = new Svm(data)
+    const bias = 1.363618234217184
+    const weights = [
+        -0.032274505797868946,
+        -0.021486578304954727,
+        -0.0542787381049902
+    ]
+    const SVM = new Svm(weights, bias)
     const input: number[] = []
     expect(() => SVM.predict(input)).toThrowError(Error("The number of inputs is less than expected"))
-});
+})
+;
 
 it('should throw error - greater', function () {
-    const data: SvmData = {
-        bias: 1.363618234217184,
-        wheits: [
-            -0.032274505797868946,
-            -0.021486578304954727,
-            -0.0542787381049902
-        ]
-    }
-    const SVM = new Svm(data)
+    const bias = 1.363618234217184
+    const weights = [
+        -0.032274505797868946,
+        -0.021486578304954727,
+        -0.0542787381049902
+    ]
+
+    const SVM = new Svm(weights, bias)
     const input = [1, 1, 1, 1]
     expect(() => SVM.predict(input)).toThrowError(Error("The number of inputs is greater than expected"))
 });
