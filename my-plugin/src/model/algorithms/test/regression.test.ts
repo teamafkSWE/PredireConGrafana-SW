3e601ca86f5fbf565fd65ec4076d9be16c129fd4
Simone <simone.meneghin.98@outlook.it>
2020-07-01 16:27:41 +0200

    aggiornati test

--- my-plugin/src/model/algorithms/test/regression.test.ts
+++ my-plugin/src/model/algorithms/test/regression.test.ts
@@ -33,3 +33,14 @@ it('RL test input exceed', function () {
     const input = [40, 25, 1]
     expect(() => RL.predict(input)).toThrowError(Error("The number of inputs is greater than expected"))
 });
+
+it('RL test input inferior', function () {
+    const b = -4.103581234222119
+    const a = [
+        0.08640900619401126,
+        0.08760164313749375
+    ]
+    const RL = new Regression(a, b)
+    const input = [40]
+    expect(() => RL.predict(input)).toThrowError(Error("The number of inputs is less than expected"))
+});
