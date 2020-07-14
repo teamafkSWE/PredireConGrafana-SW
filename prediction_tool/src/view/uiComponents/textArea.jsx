a662101acc5901e05a6d225ed35e7a1698990416
Davide Zilio <ziliodav97@gmail.com>
2020-07-14 10:43:05 +0200

    Update textArea.jsx

--- prediction_tool/src/view/uiComponents/textArea.jsx
+++ prediction_tool/src/view/uiComponents/textArea.jsx
@@ -5,7 +5,7 @@ class TextAreaNotes extends Component{
     render() {
         return(
             <div>
-                <textarea  id={"textArea"} className="w-100 p-3 bg-dark border-dark text-white" value={this.props.notes} onChange={this.props.handleNotes} placeholder='Add notes to the JSON file' />
+                <textarea id={"textArea"} className="w-100 input-group-text text-lg-left bg-dark border-dark text-white" value={this.props.notes} onChange={this.props.handleNotes} placeholder='Add notes to the JSON file' />
             </div>
         );
     }
